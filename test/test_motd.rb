0ce95a47372f0d9eb77c2ae5005bc9fc68b5929e
Eric Franz <efranz@osc.edu>
2017-03-09 22:01:00 -0500

    add failing test

--- test/test_motd.rb
+++ test/test_motd.rb
@@ -3,6 +3,7 @@ require 'tmpdir'
 
 class TestMotd < Minitest::Test
   def test_somethin
-    assert true
+    # assert_equal '/users/PZS0562/efranz/dev/rss', Dir.pwd
+    assert_equal `cat motd_rss_example.xml | ruby motd.rb`, File.open('motd_expected', 'r').read
   end
 end
