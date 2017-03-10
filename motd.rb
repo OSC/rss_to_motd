af92d7cd4d98cf3e65f21bb768d9edeb474da202
Eric Franz <efranz@osc.edu>
2017-03-09 22:21:27 -0500

    fix everything except order

--- motd.rb
+++ motd.rb
@@ -1,8 +1,28 @@
 # curl https://www.osc.edu/feeds/known-issues.xml | ruby motd.rb
 
 require 'rss'
+require 'erb'
+
+welcome = <<-EOF
+************************************************************************
+Welcome to the Ohio Supercomputer Center!
+
+PLEASE SEE THE OSC SUPERCOMPUTING DOCUMENTATION
+FOR ANNOUNCEMENTS/NOTICES AND USER INFORMATION:
+http://www.osc.edu/supercomputing
+
+For questions or assistance, contact oschelp@osc.edu
+
+To check for currently known issues, please visit
+https://www.osc.edu/supercomputing/known-issues or follow @HPCNotices
+on Twitter. 
+************************************************************************
+EOF
 
 feed = RSS::Parser.parse(STDIN.read)
-puts feed.inspect
+template = ERB.new "<%= item.pubDate.strftime('%Y/%m/%d') %>\n--- <%= item.title %>\n\n<%= item.description %>\n"
+splitter = "************************************************************************\n"
 
-# inline ERB template :-P or similar
+
+puts welcome
+puts feed.items.map { |item| template.result(binding) }.join(splitter)
--- test/test_motd.rb
+++ test/test_motd.rb
@@ -3,7 +3,6 @@ require 'tmpdir'
 
 class TestMotd < Minitest::Test
   def test_somethin
-    # assert_equal '/users/PZS0562/efranz/dev/rss', Dir.pwd
-    assert_equal `cat motd_rss_example.xml | ruby motd.rb`, File.open('motd_expected', 'r').read
+    assert_equal File.open('motd_expected', 'r').read, `cat motd_rss_example.xml | ruby motd.rb`
   end
 end
