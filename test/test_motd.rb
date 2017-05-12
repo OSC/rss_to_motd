eed45d5338683dc276a6f02910a18cee6e50e59f
Eric Franz <efranz@osc.edu>
2017-05-12 18:24:34 -0400

    move test fixtures to text/fixtures/

--- test/test_motd.rb
+++ test/test_motd.rb
@@ -3,6 +3,6 @@ require 'tmpdir'
 
 class TestMotd < Minitest::Test
   def test_somethin
-    assert_equal File.open('motd_expected', 'r').read, `cat motd_rss_example.xml | ruby motd.rb`
+    assert_equal File.open('test/fixtures/motd_expected', 'r').read, `cat test/fixtures/motd_rss_example.xml | ruby motd.rb`
   end
 end
